ba599063f7c99b6223890d08080b9beaee1fd66a
mmushtaq.k <maria.mushtaq@slu.edu>
2019-11-22 23:54:06 -0600

    saving librosa java mfcc feature to txt file

--- librosa/librosa_java/src/Main.java
+++ librosa/librosa_java/src/Main.java
@@ -2,26 +2,75 @@ import Wav.WavFile;
 import Wav.WavFileException;
 import mfcc.MFCC;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, WavFileException {
 
-        //System.out.println("Hello World!");
+        String wav_folder = "/home/maria/Desktop/Flavio Repo/Computer Security Project/Project/nativeclassifier/dataset_wav";
+
+        File directory = new File(wav_folder);
+
+        FileFilter directoryFileFilter = new FileFilter() {
+            public boolean accept(File file) {
+                return file.isDirectory();
+            }
+        };
+
+        File[] directoryListAsFile = directory.listFiles(directoryFileFilter);
+        List<String> foldersInDirectory = new ArrayList<String>(directoryListAsFile.length);
+
+        for (File directoryAsFile : directoryListAsFile) {
+
+            String folder_path_wav = directoryAsFile.getPath();
+            String folder_path_feature = folder_path_wav.replace("dataset_wav","dataset_java_feature");
+
+            //make directory if not exists
+            File directory_feature = new File(folder_path_feature);
+            if (! directory_feature.exists()){
+                directory_feature.mkdirs();
+                // If you require it to make the entire directory path including parents,
+                // use directory.mkdirs(); here instead.
+            }
+
+
+            File[] fileNames = directoryAsFile.listFiles();
+
+            for(File file : fileNames) {
+
+                //Get Mfcc Features
+                float[] mfcc_feature = ExtractMfccFeature(file.getPath());
+
+                String feature_file_path = folder_path_feature + "/" + file.getName().replace(".wav",".txt");
+
+                //wite mfcc array to txt file
+                File _file = new File(feature_file_path);
+                BufferedWriter writer = new BufferedWriter(new FileWriter(_file.getAbsolutePath()));
+
+                writer.write(Arrays.toString(mfcc_feature));
+                writer.close();
+
+                }
+            }
+
+        }
 
 
 
-        myMethod();
-
-    }
-
-    static void myMethod() throws IOException, WavFileException {
 
 
-        String strFilename  = "AAMNG1001.wav";
+
+
+    static float[] ExtractMfccFeature(String file_path) throws IOException, WavFileException {
+
+
+
+
+        String strFilename  = file_path;
 
 
         // Open the wav file specified as the path
@@ -29,7 +78,7 @@ public class Main {
         WavFile wavFile = WavFile.openWavFile(file);
 
         // Display information about the wav file
-        wavFile.display();
+        //wavFile.display();
 
         // Get the number of audio channels in the wav file
         int numChannels = wavFile.getNumChannels();
@@ -49,7 +98,7 @@ public class Main {
         MFCC Obj_Mfcc = new MFCC();
         float[] mfcc_feature =  Obj_Mfcc.process(buffer);
 
-        System.out.println(Arrays.toString(mfcc_feature));
+        return mfcc_feature;
 
     }
 }
